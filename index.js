a9d1e2bc78a1f83f95231e19ebbd35acc403eb9d
Tejas Lotlikar <tejas.lotlikar@infuse.it>
2024-07-14 00:15:25 +0530

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("zzzz")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`listening on ${process.env.SERVER}:${process.env.PORT}`)
+})
